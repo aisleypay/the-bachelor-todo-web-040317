4bf258a4ffd6ed2e49f391f921baf0d958297ad6
Paisley Singh <paisley.singh@gmail.com>
2017-03-29 22:50:12 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,73 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each { |title_season, contestants|
+
+    if title_season == season
+      contestants.each { |contestant_info|
+
+        contestant_info.each { |attribute, info|
+          return contestant_info["name"].split(" ")[0] if attribute == "status" && info == "Winner"
+        }
+      }
+    end
+  }
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each { |title_season, contestants|
+
+    contestants.each { |contestant_info|
+
+      contestant_info.each { |attribute, info|
+        return contestant_info["name"] if attribute == "occupation" && info == occupation
+      }
+    }
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  hometown_count = 0
+  data.each { |title_season, contestants|
+
+    contestants.each { |contestant_info|
+
+      contestant_info.each { |attribute, info|
+        hometown_count += 1 if attribute == "hometown" && info == hometown
+      }
+    }
+  }
+
+  hometown_count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each { |title_season, contestants|
+
+    contestants.each { |contestant_info|
+
+      contestant_info.each { |attribute, info|
+        return contestant_info["occupation"] if attribute == "hometown" && info == hometown
+      }
+    }
+  }
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  num_of_contestants = 0
+  age_sum = 0
+  data.each { |title_season, contestants|
+
+    if title_season == season
+      contestants.each { |contestant_info|
+        num_of_contestants += 1
+
+        contestant_info.each { |attribute, info|
+          age_sum += contestant_info["age"].to_f if attribute == "age"
+        }
+      }
+    end
+  }
+
+  return (age_sum / num_of_contestants).round
 end
